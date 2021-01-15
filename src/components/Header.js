a1d603027099807cfdbaddd8193ea6801d7472ae
Berik <berik_22-98@mail.ru>
2021-01-15 17:06:41 +0500

    add project react-1

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+
+        <header className="header">
+            <img src="https://www.vippng.com/png/full/39-398445_blue-lion-logo-png-lion-blue-png.png"/>
+        </header>
+    );
+}
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () =>{
+    return(
+        <nav className="nav">
+            <div className="nav__link"><a>Profile</a></div>
+            <div className="nav__link"><a>Messages</a></div>
+            <div className="nav__link"><a>News</a></div>
+            <div className="nav__link"><a>Music</a></div>
+            <div className="nav__link"><a>Settings</a></div>
+        </nav>
+    );
+}
+export default Navbar;
